3807b1f26e55db9221fcd76bbbec5d32b5d7e7f8
Kobutaemon <isotoa1219@gmail.com>
2023-11-05 13:43:53 +0900

    入力欄の判定追加

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 $(".edit-btn").click(
     function () {
         userName = window.prompt("名前を入力してください", "")
+        if (userName != null) {
+            $(".student-comment h2").text(userName);
+        } else {
+            alert("入力欄が空白です")
+        }
         info = window.prompt("書き込む内容を入力してください", "")
-        $(".student-comment h2").text(userName);
-        $(".student-comment p").text(info);
+        if (info != null) {
+            $(".student-comment h2").text(userName);
+        } else {
+            alert("入力欄が空白です")
+        }
     })
